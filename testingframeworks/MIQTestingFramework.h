dca2b8aa8315de0f89f91c0700dfd6f675593eab
David Hardiman <dhardiman@mobileiq.com>
2013-02-13 15:05:29 +0000

    Add TEST_CASE macros

--- testingframeworks/MIQTestingFramework.h
+++ testingframeworks/MIQTestingFramework.h
@@ -8,9 +8,21 @@
 
 #import <Foundation/Foundation.h>
 
+#ifdef __OBJC__
 #define EXP_SHORTHAND
 #import "Expecta.h"
 #define LRMOCKY_SUGAR
 #import "LRMocky.h"
 #import "OCMock.h"
 #import "OHHTTPStubs.h"
+
+#define TEST_CASE_WITH_SUBCLASS(name, subclass) \
+@interface name : subclass \
+@end \
+@implementation name \
+
+#define TEST_CASE(name) TEST_CASE_WITH_SUBCLASS(name, SenTestCase)
+
+#define END_TEST_CASE \
+@end
+#endif
