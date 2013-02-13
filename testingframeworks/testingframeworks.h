986d5de069841fcee1d28ebf6cdfc45b4e2118a9
David Hardiman <dhardiman@mobileiq.com>
2013-02-13 11:25:33 +0000

    Initial Commit

---
+++ testingframeworks/testingframeworks.h
@@ -0,0 +1,13 @@
+//
+//  testingframeworks.h
+//  testingframeworks
+//
+//  Created by David Hardiman on 13/02/2013.
+//  Copyright (c) 2013 Mobile IQ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface testingframeworks : NSObject
+
+@end
---
+++ testingframeworks/testingframeworks.m
@@ -0,0 +1,13 @@
+//
+//  testingframeworks.m
+//  testingframeworks
+//
+//  Created by David Hardiman on 13/02/2013.
+//  Copyright (c) 2013 Mobile IQ. All rights reserved.
+//
+
+#import "testingframeworks.h"
+
+@implementation testingframeworks
+
+@end
